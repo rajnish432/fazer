bae87910c36778ca89bce601e3b5181f9424ff89
rajnish432 <59166637+rajnish432@users.noreply.github.com>
2020-01-02 16:59:34 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,24 @@
+public class power 
+{
+	public static void main(String[]args)
+	{
+		int j=0;
+		int power=1;
+		
+			int N=Integer.parseInt(args[0]);
+			if(N>0 && N<=31)
+			{
+			while(j<=N)
+			{
+				System.out.println("2^"+j+" ="+power);
+			    power=power*2;
+				j++;			
+			}
+			}
+			else
+			{
+				System.out.println("overflows as int limit is 31");
+			}
+		
+	}
+}
